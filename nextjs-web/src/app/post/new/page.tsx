2f2cf9b6248ec791ad6c2543be8f07bb3b1a7a79
Kaviraj Jagadeesan <kavirajmailz@gmail.com>
2024-10-23 09:33:02 +0530

    [FEAT] - Search and select subreddit

--- nextjs-web/src/app/post/new/page.tsx
+++ nextjs-web/src/app/post/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useAuth } from "@/app/context/authContext";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { SubReddit } from "@/lib/data-types";
 import { getFollwedSubreddits } from "@/lib/subreddit";
 import React, { useEffect, useState, useRef } from "react";
@@ -9,13 +10,17 @@ type Props = {};
 
 const NewPost = (props: Props) => {
   const [subreddits, setSubreddits] = useState<SubReddit[]>([]);
+
+  const [filteredSubreddits, setFilteredSubreddits] = useState<SubReddit[]>([]);
   const [selectedsubreddit, setSelectedSubreddit] = useState<SubReddit | null>(
     null
   );
-  const { user, token } = useAuth();
+
+  const { token } = useAuth();
   const [showCommunities, setShowCommunities] = useState<boolean>(false);
 
   const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const searchSubredditInputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     const fetchFollwedSubreddits = async () => {
@@ -24,6 +29,7 @@ const NewPost = (props: Props) => {
           const response = await getFollwedSubreddits(token);
           const subReddits = response?.data;
           setSubreddits(subReddits ?? []);
+          setFilteredSubreddits(subReddits ?? []);
         }
       } catch (error) {
         console.log(error);
@@ -45,6 +51,7 @@ const NewPost = (props: Props) => {
 
     if (showCommunities) {
       document.addEventListener("mousedown", handleClickOutside);
+      searchSubredditInputRef.current?.focus();
     }
 
     return () => {
@@ -52,29 +59,48 @@ const NewPost = (props: Props) => {
     };
   }, [showCommunities]);
 
+  const handleSubredditSearch = () => {
+    if (searchSubredditInputRef.current?.value) {
+      const inputValue = searchSubredditInputRef.current.value.toLowerCase();
+      const _filteredSubreddits = subreddits.filter((subreddit) =>
+        subreddit.name.toLowerCase().startsWith(inputValue)
+      );
+      setFilteredSubreddits(_filteredSubreddits);
+    } else {
+      setFilteredSubreddits(subreddits);
+    }
+  };
+
   return (
     <div>
       <h1 className="bold text-2xl">Create post</h1>
 
       {/* Select Community */}
       <div className="mt-2">
-        <Button onClick={() => setShowCommunities(true)}>
-          <span className="rounded-full bg-white text-black w-6 h-6 mr-1.5">
-            r/
-          </span>
-          {selectedsubreddit?.name ?? "Select Community"}
-        </Button>
+        {!showCommunities && (
+          <Button onClick={() => setShowCommunities(true)}>
+            <span className="rounded-full bg-white text-black w-6 h-6 mr-1.5">
+              r/
+            </span>
+            {selectedsubreddit?.name ?? "Select Community"}
+          </Button>
+        )}
         {showCommunities && (
-          <div ref={dropdownRef} className="z-20 scroll-auto">
+          <div ref={dropdownRef} className="">
+            <Input
+              onChange={handleSubredditSearch}
+              ref={searchSubredditInputRef}
+              className="max-w-40"
+            />
             <ul>
-              {subreddits.map((subreddit) => (
+              {filteredSubreddits.map((subreddit) => (
                 <li
                   key={subreddit.id}
                   onClick={() => {
                     setSelectedSubreddit(subreddit);
-                    setShowCommunities(false); // Close dropdown after selection
+                    setShowCommunities(false);
                   }}
-                  className="cursor-pointer"
+                  className="cursor-pointer hover:bg-white hover:text-black max-w-40 mt-1.5 py-0.5 px-1"
                 >
                   {subreddit.name}
                 </li>
